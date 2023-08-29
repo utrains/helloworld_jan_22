c0440f90012d0efdceb608f7fa32a0118e14c3a7
mabbalde30 <mabbalde30@gmail.com>
2023-08-29 18:02:46 -0400

    ticket number

---
+++ inv.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("free -h")
+os.system("pwd")
+os.system("lscpuimport os
+
+os.system("free -h")
+os.system("pwd")
+os.system("w")
+os.system("cat /etc/passwd")
+os.system("date")
+os.system("cat /etc/passwd")
+os.system("date")
